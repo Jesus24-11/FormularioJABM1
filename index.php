e905b59cfaa44fdf7daa5f1acc6169701e94762c
Jesus24-11 <jessusbolivar1@gmail.com>
2020-03-26 14:35:59 -0430

    mi primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+<strong>Bienvenidos.....</strong><br>
+<ul>
+	<li><a href="Formulario.html">Formulario</a></li>
+	<li><a href="#">Módulo 2</a></li>
+	<li><a href="#">Módulo 3</a></li>
+</ul>
+</body>
+</html>
